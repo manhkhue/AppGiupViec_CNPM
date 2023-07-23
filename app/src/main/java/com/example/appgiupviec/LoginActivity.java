a34f6adb9982de394d197c3687c9175c319e4620
Trung Hao <haobt76>
2023-07-23 12:57:46 +0700

    hao

--- app/src/main/java/com/example/appgiupviec/LoginActivity.java
+++ app/src/main/java/com/example/appgiupviec/LoginActivity.java
@@ -4,53 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editTextLoginSDT;
-    private EditText editTextLoginPassword;
-    private Button buttonRegister;
-
-    private Button buttonLogin;
+    private TextInputEditText edSdtLogin, edPassLogin;
+    private Button btnLogin;
+    private TextView tvDkiDn, tvQuenMK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editTextLoginSDT = findViewById(R.id.editTextLoginSDT);
-        editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
-        buttonRegister = findViewById(R.id.buttonRegister);
+        edSdtLogin = findViewById(R.id.ed_sdt_login);
+        edPassLogin = findViewById(R.id.ed_pass_login);
+        btnLogin = findViewById(R.id.btn_login);
+        tvDkiDn = findViewById(R.id.tv_dki_DN);
+        tvQuenMK = findViewById(R.id.tv_quenMK_DN);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String phoneNumber = edSdtLogin.getText().toString().trim();
+                String password = edPassLogin.getText().toString().trim();
 
-                String email = editTextLoginSDT.getText().toString().trim();
-                String password = editTextLoginPassword.getText().toString().trim();
-
-
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(phoneNumber) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin đăng nhập", Toast.LENGTH_SHORT).show();
                 } else {
+                    if (phoneNumber.equals("sodienthoai") && password.equals("matkhau")) {
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                        //Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        //startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
 
-                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
+                    }
                 }
             }
         });
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
+
+        tvDkiDn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        tvQuenMK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/appgiupviec/RegisterActivity.java
+++ app/src/main/java/com/example/appgiupviec/RegisterActivity.java
@@ -1,50 +1,62 @@
 package com.example.appgiupviec;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText editTextUsername;
-    private EditText editTextSDT;
-    private EditText editTextPassword;
-    private Button buttonRegister;
+    private TextInputEditText edTenDangKy, edPassDangKy, edNhaclaiMK;
+    private Button btnDangKy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextSDT = findViewById(R.id.editTextSDT);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonRegister = findViewById(R.id.buttonRegister);
+        edTenDangKy = findViewById(R.id.ed_tenDN_dangky);
+        edPassDangKy = findViewById(R.id.ed_pass_dangky);
+        edNhaclaiMK = findViewById(R.id.ed_nhaclaiMK);
+        btnDangKy = findViewById(R.id.btn_dangky);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_login);
+        setSupportActionBar(toolbar);
 
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
-                String username = editTextUsername.getText().toString().trim();
-                String sdt = editTextSDT.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = edTenDangKy.getText().toString().trim();
+                String password = edPassDangKy.getText().toString().trim();
+                String confirmPassword = edNhaclaiMK.getText().toString().trim();
 
-
-                if (username.isEmpty() || sdt.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(phoneNumber) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập đầy đủ thông tin đăng ký", Toast.LENGTH_SHORT).show();
                 } else {
 
-                    Toast.makeText(RegisterActivity.this, "Đăng kí tài khoản thành công!", Toast.LENGTH_SHORT).show();
-
-
-                    finish(); // Kết thúc hoạt động đăng kí và quay về màn hình trước đó.
+                    if (password.equals(confirmPassword)) {
+                        Toast.makeText(RegisterActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Xác nhận mật khẩu không khớp", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
     }
 }
-
