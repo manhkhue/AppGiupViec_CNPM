1368d41eec111d2f4a98eb700569fac4d9885c11
Trung Hao <haobt76>
2023-07-23 23:25:45 +0700

    hao

--- app/src/main/java/com/example/appgiupviec/DatApp2Activity.java
+++ app/src/main/java/com/example/appgiupviec/DatApp2Activity.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class DatApp2Activity extends AppCompatActivity {
 
     private ImageView imgBack;
+    private Button btnDangViec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dat_app2);
         imgBack = findViewById(R.id.img_BackDH2);
+        btnDangViec = findViewById(R.id.btnDangViec);
+
+        btnDangViec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
